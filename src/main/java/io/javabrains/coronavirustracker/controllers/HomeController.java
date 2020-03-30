77a6ddde1edaf5640016532804ff2cd66cdacef8
rstriquer <rstriquer@gmail.com>
2020-03-29 22:21:46 -0300

    Layout Adjustments
    - Improvements in the visual and layout of elements

--- src/main/java/io/javabrains/coronavirustracker/controllers/HomeController.java
+++ src/main/java/io/javabrains/coronavirustracker/controllers/HomeController.java
@@ -1,10 +1,32 @@
 package io.javabrains.coronavirustracker.controllers;
 
+import io.javabrains.coronavirustracker.modules.LocationStats;
+import io.javabrains.coronavirustracker.services.CoronavirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.text.AttributedString;
+import java.util.List;
 
 @Controller
 public class HomeController {
-    public String home() {
+
+    @Autowired
+    CoronavirusDataService coronavirusDataService;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        List<LocationStats> allStats = coronavirusDataService.getAllStats();
+        int totalReportCases = allStats.stream().mapToInt(stat -> {
+            return stat.getLatestTotalCases();
+        }).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> {
+            return stat.getDiffFromPrevDay();
+        }).sum();
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalReportCases);
+        model.addAttribute("totalNewCases", totalNewCases);
         return "home";
     }
 }
--- src/main/java/io/javabrains/coronavirustracker/services/CoronaVirusDataService.java
+++ src/main/java/io/javabrains/coronavirustracker/services/CoronaVirusDataService.java
@@ -42,7 +42,6 @@ public class CoronaVirusDataService {
             locationStats.setState(record.get("Province/State"));
             locationStats.setCountry(record.get("Country/Region"));
             locationStats.setLatestTotalCases(Integer.parseInt(record.get(record.size() - 1)));
-            System.out.println(locationStats);
             newStats.add(locationStats);
         }
         this.allStats = newStats;
